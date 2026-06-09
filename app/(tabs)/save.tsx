c946c1c2f4265104be5bd4175aaab9de8256358a
Jonathan Concepcion <jonathanconcepcion1991@gmail.com>
2026-06-09 13:01:31 +0800

    Add tab navigation and update app icons and images
    - Implement tab navigation with screens for Home, Search, Save, and Profile.
    - Update app.json to use new logo and remove old icons.
    - Add new assets for icons and images.
    - Create interfaces for movie data structures.

---
+++ app/(tabs)/save.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const Save = () => {
+  return (
+    <View>
+      <Text>Saved</Text>
+    </View>
+  )
+}
+
+export default Save
+
+const styles = StyleSheet.create({})
