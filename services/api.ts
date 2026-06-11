32c0f960f19f50ecbfdbdfa56c442184fe27d254
Jonathan Concepcion <j.concepcion@arcanys.com>
2026-06-11 09:47:23 +0800

    Add SearchBar component and integrate it into the main tab layout, implement API configuration and custom fetch hook

---
+++ services/api.ts
@@ -0,0 +1,27 @@
+export const TMBDB_CONFIG = {
+  BASE_URL: "https://api.themoviedb.org/3",
+  API_KEY: process.env.EXPO_PUBLIC_MOVIE_API_KEY,
+  headers: {
+    accept: "application/json",
+    Authorization: `Bearer ${process.env.EXPO_PUBLIC_MOVIE_API_KEY}`,
+  },
+};
+
+export const fetchMovies = async ({ query }: { query: string }) => {
+  const endpoint = query
+    ? `${TMBDB_CONFIG.BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+    : `${TMBDB_CONFIG.BASE_URL}/discover/movie?sort_by=popularity.desc`;
+  const response = await fetch(endpoint, {
+    method: "GET",
+    headers: TMBDB_CONFIG.headers,
+  });
+
+  if (!response.ok) {
+    // @ts-ignore
+    throw new Error("Failed to fetch movies", response.statusText);
+  }
+
+  const data = await response.json();
+
+  return data.results;
+};
